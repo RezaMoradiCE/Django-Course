398adfb3b7b48a3a92897a5f05648b20fbee8124
Reza Moradi <81581274+RezaMoradiCE@users.noreply.github.com>
2024-02-24 22:41:41 +0330

    forwarding urls to index.html via render

--- website/views.py
+++ website/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'website/index.html')
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'website/about.html')
 
 def contact(request):
-    return HttpResponse('<h1>Contact</h1>')
+    return render(request, 'website/contact.html')
