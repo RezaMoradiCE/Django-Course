3cf5f98fe413bfc2c5d524b838776a695d382738
Reza Moradi <81581274+RezaMoradiCE@users.noreply.github.com>
2024-03-20 21:40:06 +0330

    image in models
    adding image field in blog.models

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+
 class Post(models.Model):
-    # image =
-    author = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
+    image = models.ImageField(upload_to='blog/', default= 'blog/default.jpg')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    # tags = 
-    # category = 
+    # tags =
+    # category =
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
-     return self.title
- 
+        return self.title
+
     class Meta:
         ordering = ['-created_date']
